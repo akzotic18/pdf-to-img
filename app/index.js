41d8c915b5738049a219191e9d745ec8cd5aa087
Clarc Magbujos <c.magbujos@jobtarget.com>
2025-05-07 03:47:46 +0800

    Added usage for density

--- app/index.js
+++ app/index.js
@@ -1,15 +1,30 @@
 const { fromPath } = require("pdf2pic");
 const fs = require("fs");
 const pdfParse = require("pdf-parse");  // Add pdf-parse to extract the number of pages
+const defaultDensity = 600;
+
+var _density;
+
 
 if (process.argv.length < 3) {
-  console.log("USAGE: node index.js [path/file.pdf]\n");
+  console.log("USAGE: node index.js [path/file.pdf]\nUSAGE: node index.js [path/file.pdf] density");
   return 1;
+}
+
+if (process.argv.length == 3) {
+  _density = defaultDensity;
+}
+
+if (process.argv.length == 4) {
+  try {
+    _density = process.argv[3].toString();
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
 // Step 1: Read the PDF and get the number of pages
 const pdfFilePath = process.argv[2].toString();
-
 
 fs.readFile(pdfFilePath, (err, data) => {
   if (err) {
@@ -18,16 +33,19 @@ fs.readFile(pdfFilePath, (err, data) => {
   }
 
   pdfParse(data).then((pdfData) => {
+
     const totalPages = pdfData.numpages; // Get the number of pages in the PDF
+
+    console.log('DENSITY: ', _density);
 
     // Step 2: Create the converter
     const convert = fromPath(pdfFilePath, {
-      density: 600,                     // quality/resolution
+      density: _density,                     // quality/resolution
       saveFilename: "page",
       savePath: "./images",
       format: "png",
       // width: 1024,                      // set only width
-      preserveAspectRatio: true               // output format
+      preserveAspectRatio: true,               // output format
     });
 
     // Step 3: Loop through each page and convert
